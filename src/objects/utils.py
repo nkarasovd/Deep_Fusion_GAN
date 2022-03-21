fe4ff463e40dacd7182b285382f28163b7408a55
Nikolai <nikolai.karasoff@yandex.ru>
2022-03-21 11:06:33 +0300

    data -> batch

--- src/objects/utils.py
+++ src/objects/utils.py
@@ -4,9 +4,9 @@ import torch
 from torch import Tensor
 
 
-def prepare_data(data: Tuple[Tensor, Tensor, Tensor, Tuple[str]],
+def prepare_data(batch: Tuple[Tensor, Tensor, Tensor, Tuple[str]],
                  device: torch.device) -> Tuple[Tensor, Tensor, Tensor, List[str]]:
-    images, captions, captions_lens, file_names = data
+    images, captions, captions_lens, file_names = batch
 
     sorted_cap_lens, sorted_cap_indices = torch.sort(captions_lens, 0, True)
     sorted_cap_lens = sorted_cap_lens.to(device)
