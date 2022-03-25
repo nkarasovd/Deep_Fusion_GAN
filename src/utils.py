5d4eceb4d7d225d160d300dedf4ff02acd6dd5e8
Nikolai <nikolai.karasoff@yandex.ru>
2022-03-25 11:32:06 +0300

    Add plot functions

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import random
+from typing import List, Tuple
 
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
@@ -30,3 +32,48 @@ def fix_seed(seed: int = 123321):
         torch.cuda.manual_seed_all(seed)
 
     print(f"Seed {seed} fixed")
+
+
+def plot_losses(g_losses: List[float], d_losses: List[float], d_gp_losses: List[float],
+                path_save: str = "losses.png"):
+    plt.style.use("seaborn")
+
+    plt.figure(dpi=256)
+
+    plt.plot(g_losses, label="G loss")
+    plt.plot(d_losses, label="D loss")
+    plt.plot(d_gp_losses, label="D MA-GP loss")
+
+    plt.xlabel("Number of epochs")
+    plt.ylabel("Loss value")
+
+    plt.legend()
+
+    plt.title("DF-GAN losses")
+
+    plt.tight_layout()
+    plt.savefig(path_save)
+    plt.show()
+
+
+def plot_metrics(fid: List[float], iscore: List[float], epochs: Tuple[int],
+                 path_save: str = "metrics.png"):
+    plt.style.use("seaborn")
+    
+    plt.figure(dpi=256)
+
+    plt.plot(fid, label="FID")
+    plt.plot(iscore, label="Inception Score")
+
+    plt.xticks(np.arange(len(epochs)), epochs)
+
+    plt.xlabel("Epoch")
+    plt.ylabel("Metric value")
+
+    plt.legend()
+
+    plt.title("Deep Fusion GAN metrics values per epochs")
+
+    plt.tight_layout()
+    plt.savefig(path_save)
+    plt.show()
